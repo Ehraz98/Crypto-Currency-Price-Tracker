ef8fb70e39ac7b53cf4da78eecb66c7e9cc37bda
Ehraz98 <ehrazahmed14@gmail.com>
2023-01-01 13:12:56 +0530

    Crypto Checker

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CoinDetailComponent } from './coin-detail/coin-detail.component';
+import { CoinListComponent } from './coin-list/coin-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'coin-list',
+    pathMatch: 'full',
+  },
+  { path: 'coin-list', component: CoinListComponent },
+  { path: 'coin-detail/:id', component: CoinDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { CurrencyService } from './service/currency.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'crypto-checker';
+  projectTitle: string = 'Crypto Checker';
+  selectedCurrency: string  = 'INR';
+  constructor(private currencyService : CurrencyService){
+
+  }
+  sendCurrency(event:string){
+    this.currencyService.setCurrency(event);
+  }
 }
